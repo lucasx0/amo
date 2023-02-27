1e837e8d9be02f236f07deea9adf7c1d65da26e6
lucasx0 <lucas.almeidasilva49@gmail.com>
2023-02-27 14:43:43 -0300

    criando component dashboard e adiciona no index das routes.

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,15 @@ import Route from './Route';
 //pages
 import SignIn from '../pages/SignIn/signIn';
 import SignUp from '../pages/SignUp/signUp';
+import Dashboard from '../pages/Dashboard/dashboard';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={SignIn} />
       <Route exact path="/register" component={SignUp} />
+
+      <Route exact path="/dashboard" component={Dashboard} isPrivate />
     </Switch>
   );
 }
