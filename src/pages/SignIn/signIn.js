694d410a03a1efd32490716471b029a66cb68288
lucasx0 <lucas.almeidasilva49@gmail.com>
2023-02-27 15:17:08 -0300

    enviando pagina de login.

--- src/pages/SignIn/signIn.js
+++ src/pages/SignIn/signIn.js
@@ -1,7 +1,45 @@
-export default function SignIn() {
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo1.png';
+import './signin.css';
+
+function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    alert('Clicou'); //teste de botao
+  }
+
   return (
-    <div>
-      <h1>TELA DE LOGIN</h1>
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="Sistema Logo" />
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <h1>Acesso ao Escritório Virtual</h1>
+          <input
+            type="text"
+            placeholder="Seu Login"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Sua Senha"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit">Acessar</button>
+        </form>
+
+        <Link to="/register">Criar uma conta</Link>
+      </div>
     </div>
   );
 }
+
+export default SignIn;
